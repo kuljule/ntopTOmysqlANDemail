187bea4e88cca81b316a24131bb2dc96a2f4340a
Julian <julianrseidel@gmail.com>
2017-01-10 11:53:18 -0800

    Update Add username, password, database

--- ntopv15.sh
+++ ntopv15.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+username=""     # input username
+password=""     # input password
+database=""     # input database
+
 wget -O '/home/mysqlv10/Downloads/ntopv15.xml' "http://192.168.3.254:3000/dumpData.html?language=xml&view=long"		# download the ntop data dump as an xml file
-mysql -u root -padmkn2008 bandwidth < '/home/mysqlv10/ntopv15.txt'			# connect to mysql database and run commands in ntopv15.txt
+mysql -u $username -$password $database < '/home/mysqlv10/ntopv15.txt'			# connect to mysql database and run commands in ntopv15.txt
 
 # ntopv15.txt loads ntopv15.xml into a table called ntopv15
